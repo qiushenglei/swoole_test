1cb1e8c321853a240027147be05b7111e5f34591
qiushenglei <497205842@qq.com>
2018-06-21 09:55:28 +0800

    async

--- tcp.php
+++ tcp.php
@@ -4,16 +4,17 @@ $serv = new swoole_server("127.0.0.1", 9501);
 //设置异步任务的工作进程数量
 $serv->set(array('task_worker_num' => 4));
 
-$serv->on('receive', function($serv, $fd, $from_id, $data) {
+$serv->on('receive', function ($serv, $fd, $from_id, $data) {
     //投递异步任务
     $task_id = $serv->task($data);
     echo "Dispath AsyncTask: id=$task_id\n";
+    var_dump($serv->stats());
     $serv->send($fd, "addTask success");
 });
 
 //处理异步任务
 $serv->on('task', function ($serv, $task_id, $from_id, $data) {
-    echo "New AsyncTask[id=$task_id]".PHP_EOL;
+    echo "New AsyncTask[id=$task_id]" . PHP_EOL;
     var_dump($data);
     //返回任务执行的结果
     $serv->finish("$data -> OK");
@@ -21,10 +22,10 @@ $serv->on('task', function ($serv, $task_id, $from_id, $data) {
 
 //处理异步任务的结果
 $serv->on('finish', function ($serv, $task_id, $data) {
-    for($i=0;$i<1000000;$i++){
+    for ($i = 0; $i < 1000000; $i++) {
 
     }
-    echo "AsyncTask[$task_id] Finish: $data".PHP_EOL;
+    echo "AsyncTask[$task_id] Finish: $data" . PHP_EOL;
 });
 
 $serv->start();
