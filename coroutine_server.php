603c733fc629a83e9de701d8835af0ced523b107
qiushenglei <497205842@qq.com>
2018-06-22 11:16:44 +0800

    async

--- coroutine_server.php
+++ coroutine_server.php
@@ -1,6 +1,7 @@
 <?php
 $serv = new swoole_server("127.0.0.1", 8888);
-$serv->on('receive', function ($serv, $fd, $from_id, $data) {
-    $serv->send($fd, $data);
+$serv->on('receive', function ($serv3, $fd, $from_id, $data) {
+    var_dump($serv3);
+    $serv3->send($fd, $data);
 });
 $serv->start();
--- http_server.php
+++ http_server.php
@@ -4,6 +4,7 @@ $http = new swoole_http_server("0.0.0.0", 9501);
 $http->on("request", function ($request, $response) {
     $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
     $client->connect("127.0.0.1", 8888, 0.5);
+    var_dump($client);
     //调用connect将触发协程切换
     $client->send("hello world from swoole");
     //调用recv将触发协程切换
