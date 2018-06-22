c0c1f8a1cb547aefcaf2ced0d0e007679de4aa13
qiushenglei <497205842@qq.com>
2018-06-22 11:05:00 +0800

    async

--- coroutine_server.php
+++ coroutine_server.php
@@ -1,8 +1,6 @@
 <?php
 $serv = new swoole_server("127.0.0.1", 8888);
 $serv->on('receive', function ($serv, $fd, $from_id, $data) {
-
-    echo "Dispath AsyncTask: id=$data";
-
+    $serv->send($data);
 });
 $serv->start();
