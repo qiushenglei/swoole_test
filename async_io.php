76dd2ab19ff0be47fd25df6904324818d8cf9ed2
qiushenglei <497205842@qq.com>
2018-06-22 17:36:46 +0800

    test

--- async_io.php
+++ async_io.php
@@ -1,13 +1,13 @@
 <?php
     use Swoole\Async;
-    Async::readFile(__DIR__."/tcp.php", function($a, $b){
+    Async::read(__DIR__."/tcp.php", function($a, $b){
         echo $a;
         echo $b;
     });
 
     echo "read end"; //先输出的end,代表了是异步打开了文件
 
-    Async::writeFile(__DIR__."/write_test.php", "write test", "write_call_back", 1);
+    Async::write(__DIR__."/write_test.php", "write test", "write_call_back", 1);
 
     function write_call_back($a, $b){
         echo "{$a} {$b}write success";
