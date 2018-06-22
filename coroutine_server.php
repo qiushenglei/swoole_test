ae3bfe1b7fb670765edf7733151627d6b076e39e
qiushenglei <497205842@qq.com>
2018-06-22 11:25:03 +0800

    async

--- coroutine_server.php
+++ coroutine_server.php
@@ -1,7 +1,6 @@
 <?php
 $serv = new swoole_server("127.0.0.1", 8888);
-$serv->on('receive', function ($serv3, $fd, $from_id, $data) {
-    var_dump($serv3);
-    $serv3->send($fd, $data.$fd);
+$serv->on('receive', function (\Swoole\Server $serv3, $fd, $from_id, $data) {
+    $serv3->send($fd, $data.$fd." {$serv3->worker_id}\n");
 });
 $serv->start();
