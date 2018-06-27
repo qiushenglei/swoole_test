2046c26e9916252bf71a74cbc606a41d6fd3684d
qiushenglei <497205842@qq.com>
2018-06-27 16:35:17 +0800

    coro

--- coroutine_port_888_server.php
+++ coroutine_port_888_server.php
@@ -1,6 +1,10 @@
 <?php
 $serv = new swoole_server("127.0.0.1", 8888);
 $serv->on('receive', function (\Swoole\Server $serv3, $fd, $from_id, $data) {
+    for($i=0;$i<10;$i++){
+        echo $i,PHP_EOL;
+    }
+    echo $data;
     $serv3->send($fd, $data.$fd." {$serv3->worker_id}\n");
 });
 $serv->start();
