8501538aa129eecad0fcb47c264d71a3b0563885
qiushenglei <497205842@qq.com>
2018-06-27 18:14:57 +0800

    coro

--- coroutine_port_9501_server.php
+++ coroutine_port_9501_server.php
@@ -7,7 +7,7 @@ $http->on("request", function ($request, $response) {
     //调用connect将触发协程切换
     $client->send("hello world from swoole");
     //调用recv将触发协程切换
-    var_dump( microtime(true));
+    var_dump( microtime(true)); //send和recv中间插入业务代码，相当于非阻塞。当需要到，异步的代码result时，再client->recv
     echo "recv after";
     $ret = $client->recv();
     echo "recv after";
