82be5b53fb983182b8ef3800138613c67bc5d1f5
qiushenglei <497205842@qq.com>
2018-07-02 17:42:32 +0800

    coro

--- coroutine_channel.php
+++ coroutine_channel.php
@@ -3,6 +3,9 @@ echo 1111;
 $b = "1111";
 go(function () {
     $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
+    for($i=0;$i<1000;$i++){
+
+    }
     echo 222;
     echo $b = "aaaa";
 //    if (!$client->connect('127.0.0.1', 9501, 0.5)) {
