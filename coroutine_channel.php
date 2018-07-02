971f6e128c29de80a9a57f231a8775863080badc
qiushenglei <497205842@qq.com>
2018-07-02 10:53:41 +0800

    coro

--- coroutine_channel.php
+++ coroutine_channel.php
@@ -21,15 +21,15 @@ use Swoole\Coroutine as co;
 //swoole_event::wait();
 go(function() {
     go(function () {
-        co::sleep(3.0);
+//        co::sleep(3.0);
         go(function () {
-            co::sleep(2.0);
+//            co::sleep(2.0);
             echo "co[3] end\n";
         });
         echo "co[2] end\n";
     });
 
-    co::sleep(1.0);
+//    co::sleep(1.0);
     echo "co[1] end\n";
 });
 ?>
