c6e43f9485755aa09aaa348217fe4a7d91f92050
qiushenglei <497205842@qq.com>
2018-07-02 11:02:31 +0800

    coro

--- coroutine_channel.php
+++ coroutine_channel.php
@@ -1,35 +1,17 @@
 <?php
 use Swoole\Coroutine as co;
 
-////echo Swoole\Coroutine::getuid();
-////$chan = new co\Channel(1);
-////co::create(function () use ($chan) {
-////    echo Swoole\Coroutine::getuid();
-////    for ($i = 0; $i < 100000; $i++) {
-////        co::sleep(1.0);
-////        $chan->push(['rand' => rand(1000, 9999), 'index' => $i]);
-////        echo "$i\n";
-////    }
-////});
-////co::create(function () use ($chan) {
-////    echo Swoole\Coroutine::getuid();
-////    while (1) {
-////        $data = $chan->pop();
-////        var_dump($data);
-////    }
-////});
-//swoole_event::wait();
-go(function() {
-    go(function () {
-//        co::sleep(3.0);
-        go(function () {
-//            co::sleep(2.0);
-            echo "co[3] end\n";
-        });
-        echo "co[2] end\n";
-    });
-
-//    co::sleep(1.0);
-    echo "co[1] end\n";
+$id = go(function(){
+    $id = co::getUid();
+    echo "start coro $id\n";
+    co::suspend();
+    echo "resume coro $id @1\n";
+    co::suspend();
+    echo "resume coro $id @2\n";
 });
+echo "start to resume $id @1\n";
+co::resume($id);
+echo "start to resume $id @2\n";
+co::resume($id);
+echo "main\n";
 ?>
