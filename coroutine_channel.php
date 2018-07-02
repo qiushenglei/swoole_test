f5b6f48f6f953c0ecaf8a56f1b54a4f9ad09e61f
qiushenglei <497205842@qq.com>
2018-07-02 13:58:41 +0800

    coro

--- coroutine_channel.php
+++ coroutine_channel.php
@@ -1,22 +1,9 @@
 <?php
-use Swoole\Coroutine as co;
-$chan = new co\Channel(1);
-co::create(function () use ($chan) {
-    for($i = 0; $i < 100000; $i++) {
-        co::sleep(0.1);
-        $chan->push(['rand' => rand(1000, 9999), 'index' => $i]);
-        echo "$i\n";
-    }
-});
-co::create(function () use ($chan) {
-    while(1) {
-        $data = $chan->pop();
-        var_dump($data);
-    }
-});
-co::create(function () use ($chan) {
-    for($i=0;$i<100;$i++){
-        echo "third coroutine";
-    }
-});
-swoole_event::wait();
+$client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
+if (!$client->connect('127.0.0.1', 9501, 0.5))
+{
+    exit("connect failed. Error: {$client->errCode}\n");
+}
+$client->send("hello world\n");
+echo $client->recv();
+$client->close();
