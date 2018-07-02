410695bebd572e510f26f4a675989ca44d41ce9d
qiushenglei <497205842@qq.com>
2018-07-02 11:30:55 +0800

    coro

--- coroutine_channel.php
+++ coroutine_channel.php
@@ -1,17 +1,17 @@
 <?php
 use Swoole\Coroutine as co;
-
-$id = go(function(){
-    $id = co::getUid();
-    echo "start coro $id\n";
-    co::suspend($id);
-    echo "resume coro $id @1\n";
-    co::suspend($id);
-    echo "resume coro $id @2\n";
+$chan = new co\Channel(1);
+co::create(function () use ($chan) {
+    for($i = 0; $i < 100000; $i++) {
+//        co::sleep(1.0);
+        $chan->push(['rand' => rand(1000, 9999), 'index' => $i]);
+        echo "$i\n";
+    }
 });
-echo "start to resume $id @1\n";
-co::resume($id);
-echo "start to resume $id @2\n";
-co::resume($id);
-echo "main\n";
-?>
+co::create(function () use ($chan) {
+    while(1) {
+        $data = $chan->pop();
+        var_dump($data);
+    }
+});
+swoole_event::wait();
