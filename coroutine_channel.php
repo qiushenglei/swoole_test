cfec4ce2ccd9d6684da8eac41bcfbf0b59d04230
qiushenglei <497205842@qq.com>
2018-07-02 17:14:39 +0800

    coro

--- coroutine_channel.php
+++ coroutine_channel.php
@@ -1,10 +1,14 @@
 <?php
+echo 1111;
 go(function () {
     $client = new Swoole\Coroutine\Client(SWOOLE_SOCK_TCP);
-    if (!$client->connect('127.0.0.1', 9501, 0.5)) {
-        exit("connect failed. Error: {$client->errCode}\n");
-    }
-    $client->send("hello world\n");
-    echo $client->recv();
-    $client->close();
+    echo 222;
+//    if (!$client->connect('127.0.0.1', 9501, 0.5)) {
+//        exit("connect failed. Error: {$client->errCode}\n");
+//    }
+//    $client->send("hello world\n");
+//    echo $client->recv();
+//    $client->close();
 });
+
+echo 3333;
