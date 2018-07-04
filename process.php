cda3b60108e61a762aba4c145f123269055e0113
qiushenglei <497205842@qq.com>
2018-07-04 10:58:03 +0800

    coro

--- process.php
+++ process.php
@@ -3,6 +3,6 @@ use Swoole\Process;
 $process = new Process(function (Process $worker) {
     $worker->exec('/usr/local/php/bin/php', ['/home/work/swoole_project/echo.php','3','sdfs','5']);
     $worker->write('hello');
-}, false); // 需要启用标准输入输出重定向
+}, true); // 需要启用标准输入输出重定向
 $process->start();
-echo "from exec: ". $process->read(). "\n";
+//echo "from exec: ". $process->read(). "\n";
