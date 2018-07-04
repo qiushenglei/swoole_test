89f0dfdd5bb636d1bef8a22ef2c9bed5e47561c7
qiushenglei <497205842@qq.com>
2018-07-04 10:51:38 +0800

    coro

--- echo.php
+++ echo.php
@@ -1,3 +1,3 @@
 <?php
-var_dump($_REQUEST);
+print_r($argv);
 ?>
--- process.php
+++ process.php
@@ -1,7 +1,7 @@
 <?php
 use Swoole\Process;
 $process = new Process(function (Process $worker) {
-    $worker->exec('/usr/local/php/bin/php', ['/home/work/swoole_project/echo.php','3','sdfs']);
+    $worker->exec('/usr/local/php/bin/php', ['/home/work/swoole_project/echo.php','3','sdfs','5']);
     $worker->write('hello');
 }, true); // 需要启用标准输入输出重定向
 $process->start();
