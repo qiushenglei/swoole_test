1e30d20f800e69df7c94f4f8da5b389f154342e5
qiushenglei <497205842@qq.com>
2018-07-05 16:53:30 +0800

    s

--- process.php
+++ process.php
@@ -1,32 +1,33 @@
 <?php
-//$serv = new swoole_server("127.0.0.1", 9501);
-//$serv->set(array(
-//    'worker_num' => 2,    //开启两个worker进程
-//    'max_request' => 3,   //每个worker进程max request设置为3次
-//    'dispatch_mode'=>3,
-//));
-////监听数据接收事件
-//$serv->on('receive', function ($serv, $fd, $from_id, $data) {
-//    $serv->send($fd, "Server: ".$data);
-//});
-////启动服务器
-//$serv->start();
 $serv = new swoole_server("127.0.0.1", 9501);
 $serv->set(array(
     'worker_num' => 2,    //开启两个worker进程
     'max_request' => 2,   //每个worker进程max request设置为3次
+    'dispatch_mode'=>3,
 ));
-$serv->on('connect', function ($serv, $fd){
-    echo "Client:Connect.\n";
-});
+//监听数据接收事件
 $serv->on('receive', function ($serv, $fd, $from_id, $data) {
-    $serv->send($fd, 'Swoole: '.$data);
-    $serv->close($fd);
+    $serv->send($fd, "Server: ".$data);
 });
-$serv->on('close', function ($serv, $fd) {
-    echo "Client: Close.\n";
-});
+//启动服务器
 $serv->start();
+
+//$serv = new swoole_server("127.0.0.1", 9501);
+//$serv->set(array(
+//    'worker_num' => 2,    //开启两个worker进程
+//    'max_request' => 2,   //每个worker进程max request设置为3次
+//));
+//$serv->on('connect', function ($serv, $fd){
+//    echo "Client:Connect.\n";
+//});
+//$serv->on('receive', function ($serv, $fd, $from_id, $data) {
+//    $serv->send($fd, 'Swoole: '.$data);
+//    $serv->close($fd);
+//});
+//$serv->on('close', function ($serv, $fd) {
+//    echo "Client: Close.\n";
+//});
+//$serv->start();
 
 
 ?>
